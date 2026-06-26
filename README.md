b499c44ce1147662de5fbb3608c0d3beaf70da89
spekie <gbarkar@proton.me>
2026-06-26 19:51:04 -0400

    add comments and fix readme

--- README.md
+++ README.md
@@ -1,25 +1,29 @@
 # userjs
 
-My `user-overrides.js` file for [arkenfox](https://github.com/arkenfox/user.js)
+My [user-overrides.js](user-overrides.js) file for [arkenfox](https://github.com/arkenfox/user.js)
 
 ## Installing
 
 Run the Mozilla Firefox web browser at least once so it can create the directories necessary to start the browser profile, run the shell scripts included in the repository as shown below.
 
-First run "clean.sh" in a shell like this:
+Clone this repository with `git`
+```sh
+git clone https://github.com/spekie/userjs.git
+```
+
+First run `clean.sh` in a shell like this:
 ```sh
 ./clean.sh
 ```
-[clean.sh](clean.sh) will clear the `mozilla` directories in `$XDG_CACHE_HOME` and `$XDG_CONFIG_HOME` it will also save the default firefox profile directory however it will clear all data, history, settings and more.
+[clean.sh](clean.sh) will clear the Firefox directories in `$XDG_CACHE_HOME` and `$XDG_CONFIG_HOME` it will also save the default Firefox profile directory however it will clear all data, history, settings and more.
 
-Then run "make.sh" in a shell like this:
+Then run `make.sh` in a shell like this:
 ```sh
 ./make.sh
 ```
-[make.sh](make.sh) will clone the [arkenfox](https://github.com/arkenfox/user.js) repository, copy the arkenfox scripts and user.js, as well as [user-overrides.js](user-overrides.js) to the default firefox profile directory
+[make.sh](make.sh) will clone the [arkenfox](https://github.com/arkenfox/user.js) repository, copy the arkenfox scripts and user.js, as well as [user-overrides.js](user-overrides.js) to the default Firefox profile directory
 
-Lastly use `cd` to go into the default firefox profile directory and run the [arkenfox](https://github.com/arkenfox/user.js) shell scripts `updater.sh` to apply the overrides and `prefsCleaner.sh` when needed.
+Lastly use `cd` to go into the default Firefox profile directory and run the [arkenfox](https://github.com/arkenfox/user.js) shell scripts `updater.sh` to apply the overrides and `prefsCleaner.sh` when needed.
 ```sh
-cd "$XDG_CONFIG_HOME"/mozilla/firefox/*.default-release/
-./updater.sh
+cd "$XDG_CONFIG_HOME"/mozilla/firefox/*.default-release/ && ./updater.sh
 ```
--- user-overrides.js
+++ user-overrides.js
@@ -1,11 +1,8 @@
 /*
  * user-overrides.js
  */
-user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
-user_pref("network.trr.mode", 5);
-user_pref("media.gmp-widevinecdm.enabled", false);
-user_pref("media.gmp-widevinecdm.visible", false);
-user_pref("datareporting.usage.uploadEnabled", false);
+
+/* about:preferences#passwordsAutofill */
 user_pref("signon.rememberSignons", false);
 user_pref("signon.management.page.breach-alerts.enabled", false);
 user_pref("signon.generation.enabled", false);
@@ -13,7 +10,8 @@ user_pref("signon.firefoxRelay.feature", "disabled");
 user_pref("dom.forms.autocomplete.formautofill", false);
 user_pref("extensions.formautofill.addresses.enabled", false);
 user_pref("extensions.formautofill.creditCards.enabled", false);
-user_pref("browser.urlbar.suggest.topsites", false);
+
+/* about:preferences#ai */
 user_pref("browser.ai.control.default", "blocked");
 user_pref("browser.ai.control.linkPreviewKeyPoints", "blocked");
 user_pref("browser.ai.control.pdfjsAltText", "blocked");
@@ -21,7 +19,15 @@ user_pref("browser.ai.control.sidebarChatbot", "blocked");
 user_pref("browser.ai.control.smartTabGroups", "blocked");
 user_pref("browser.ai.control.smartWindow", "blocked");
 user_pref("browser.ai.control.translations", "blocked");
-//user_pref("general.autoScroll", true);
-//user_pref("widget.use-xdg-desktop-portal.file-picker", 0);
-//user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-//user_pref("browser.compactmode.show", true);
+
+/* misc */
+user_pref("network.trr.mode", 5); // about:preferences#privacy use default DNS resolver
+user_pref("browser.urlbar.suggest.topsites", false); // about:preferences#search do not suggest shortcuts
+user_pref("datareporting.usage.uploadEnabled", false); // about:preferences#permissionsData data collection
+user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false); // about:preferences#tabsBrowsing media disable picture-in-picture
+user_pref("media.gmp-widevinecdm.enabled", false); // about:preferences#tabsBrowsing media disable DRM/widevine
+user_pref("media.gmp-widevinecdm.visible", false); // disable DRM/widevine prompt
+//user_pref("general.autoScroll", true); // about:preferences#accessibility use autoscrolling
+//user_pref("widget.use-xdg-desktop-portal.file-picker", 0); // do not use xdg-desktop-portal file picker
+//user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // for css themes
+//user_pref("browser.compactmode.show", true); // compact toolbar mode
